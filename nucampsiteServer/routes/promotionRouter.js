4d023b9db279e9e356569a0a04904160758c831b
JesseG <j.a.grant27@gmail.com>
2022-09-08 22:17:51 -0400

    Passport JWT

--- nucampsiteServer/routes/promotionRouter.js
+++ nucampsiteServer/routes/promotionRouter.js
@@ -1,43 +1,34 @@
 const express = require('express');
 const promotionRouter = express.Router();
+const authenticate = require('../authenticate');
 
 promotionRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req, res) => {
     res.end('Will send all the promotions to you');
 })
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
     res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
 })
-.put((req, res) => {
+.put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
-.delete((req, res) => {
+.delete(authenticate.verifyUser, (req, res) => {
     res.end('Deleting all promotions');
 });
 
 promotionRouter.route('/:promotionId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req, res) => {
     res.end(`Will send details of the capsite: ${req.params.promotionId}`)
 })
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
     res.statudCode = 403;
     res.end('POST operation not supported')
 })
-.put((req, res) => {
+.put(authenticate.verifyUser, (req, res) => {
     res.end(`Will update the promotion: ${req.body.name} with description: $ ${req.body.description}`)
 })
-.delete((req,res) => {
+.delete(authenticate.verifyUser, (req,res) => {
     res.end('Deleting promotion!');
 })
 
